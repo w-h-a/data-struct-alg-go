6985d19aee2f3c6aeded25f5b0b993f1295aef7b
Wes Anderson <wesley.herb.anderson@gmail.com>
2022-01-19 08:07:53 -0500

    modifying stacks and qs a bit

--- queues/queue-model.go
+++ queues/queue-model.go
@@ -8,30 +8,58 @@ var (
 	ErrQueueIsNil = errors.New("queue is nil")
 )
 
+/*
 type Queuer interface {
-	Enq(x int) *Queue
-	Head() (int, error)
-	Deq() (*Queue, error)
+    Enq(x int) *Queue
+    Head() (int, error)
+    Deq() (*Queue, error)
 }
+
+type Queue struct {
+    Data []int
+}
+
+func (q *Queue) Enq(x int) *Queue {
+    return &Queue{
+        Data: append(q.Data, x),
+    }
+}
+
+func (q *Queue) Head() (int, error) {
+    if q == nil || len(q.Data) == 0 {
+        return 0, ErrQueueIsNil
+    }
+    return q.Data[0], nil
+}
+
+func (q *Queue) Deq() (*Queue, error) {
+    if q == nil || len(q.Data) == 0 {
+        return nil, ErrQueueIsNil
+    }
+    return &Queue{
+        Data: q.Data[1:],
+    }, nil
+}
+*/
 
 type Queue struct {
 	Data []int
 }
 
-func (q *Queue) Enq(x int) *Queue {
+func Enq(q *Queue, x int) *Queue {
 	return &Queue{
 		Data: append(q.Data, x),
 	}
 }
 
-func (q *Queue) Head() (int, error) {
+func Head(q *Queue) (int, error) {
 	if q == nil || len(q.Data) == 0 {
 		return 0, ErrQueueIsNil
 	}
 	return q.Data[0], nil
 }
 
-func (q *Queue) Deq() (*Queue, error) {
+func Deq(q *Queue) (*Queue, error) {
 	if q == nil || len(q.Data) == 0 {
 		return nil, ErrQueueIsNil
 	}
--- stacks/stack-model.go
+++ stacks/stack-model.go
@@ -8,30 +8,58 @@ var (
 	ErrStackIsNil = errors.New("stack is nil")
 )
 
+/*
 type Stacker interface {
-	Push(x int) *Stack
-	Top() (int, error)
-	Pop() (*Stack, error)
+    Push(x int) *Stack
+    Top() (int, error)
+    Pop() (*Stack, error)
 }
+
+type Stack struct {
+    Data []int
+}
+
+func (s *Stack) Push(x int) *Stack {
+    return &Stack{
+        Data: append([]int{x}, s.Data...),
+    }
+}
+
+func (s *Stack) Top() (int, error) {
+    if s == nil || len(s.Data) == 0 {
+        return 0, ErrStackIsNil
+    }
+    return s.Data[0], nil
+}
+
+func (s *Stack) Pop() (*Stack, error) {
+    if s == nil || len(s.Data) == 0 {
+        return nil, ErrStackIsNil
+    }
+    return &Stack{
+        Data: s.Data[1:],
+    }, nil
+}
+*/
 
 type Stack struct {
 	Data []int
 }
 
-func (s *Stack) Push(x int) *Stack {
+func Push(s *Stack, x int) *Stack {
 	return &Stack{
 		Data: append([]int{x}, s.Data...),
 	}
 }
 
-func (s *Stack) Top() (int, error) {
+func Top(s *Stack) (int, error) {
 	if s == nil || len(s.Data) == 0 {
 		return 0, ErrStackIsNil
 	}
 	return s.Data[0], nil
 }
 
-func (s *Stack) Pop() (*Stack, error) {
+func Pop(s *Stack) (*Stack, error) {
 	if s == nil || len(s.Data) == 0 {
 		return nil, ErrStackIsNil
 	}
